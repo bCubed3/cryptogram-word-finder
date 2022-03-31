f3fec3a5e7307334977f00932bd94be034b9b7e1
bCubed3 <sf.bcubed@gmail.com>
2022-03-31 20:29:45 +0000

    added word lists (also changed gitignore)

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 import re
 
-inp = "abccad"
-testWords = ["little", "tweets", "hello", "abccad", "xxxxxx", "littles", "belittle"]
+#inp = input("Cryptogram word : ")
+inp="that"
+with open("eng10k.txt", "r") as f:
+  testWords = f.readlines()
 
+for w in range(len(testWords)):
+  testWords[w] = testWords[w][0:-1]
 
 def letter_of(word):
-    out = []
-    for l in word:
-        if not (l in out):
-            out.append(l)
+	out = []
+	for l in word:
+		if not (l in out):
+			out.append(l)
 
 
 def make_nl(l):
-    out = ""
-    for i in range(l):
-        out += "\\" + str(i + 1)
-    if not (l == 0):
-        out = "(?!" + out + ")"
-    return out
+	out = ""
+	for i in range(l):
+		out += "\\" + str(i + 1)
+	if not (l == 0):
+		out = "(?!" + out + ")"
+	return out
 
 
-def make_reg(word):
-    used_letters = []
-    out = "^"
-    for l in word:
-        if l in used_letters:
-            out = out + "\\{}".format(used_letters.index(l) + 1)
-        else:
-            out = out + "({}[^{}])".format(make_nl(len(used_letters)), l)
-            used_letters.append(l)
+def make_reg(word, exclude_self):
+	used_letters = []
+	out = "^"
+	for l in word:
+		if l in used_letters:
+			out = out + "\\{}".format(used_letters.index(l) + 1)
+		else:
+			if exclude_self:
+				out = out + "({}[^{}])".format(make_nl(len(used_letters)), l)
+			else:
+				out = out + "({}.)".format(make_nl(len(used_letters)))
+			used_letters.append(l)
 
-    return out + "$"
+	return out + "$"
 
 
 def match_list(r, l):
-    out = []
-    for w in l:
-        if re.match(r, w):
-            out.append(w)
-    return out
+	out = []
+	for w in l:
+		if re.match(r, w):
+			out.append(w)
+	return out
 
 
-def filter_reg(r, l):
-    return match_list(make_reg(r.lower()), l)
+def filter_reg(r, l, exclude_self=True):
+	return match_list(make_reg(r.lower(), exclude_self), l)
 
 
-print(make_reg(inp.lower()))
-print(filter_reg(inp, testWords))
+print(make_reg(inp.lower(), False))
+print(filter_reg(inp, testWords, True))
